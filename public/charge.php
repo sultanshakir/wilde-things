bb6f5a3cdcb89b081b245f6d1597c7ecabc510d5
Sultan B Shakir <contact@sultanshakir.com>
2015-07-08 11:55:49 -0400

    cleaned up bad message

--- public/charge.php
+++ public/charge.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       "A little sincerity is a dangerous thing, and a great deal of it is absolutely fatal.",
       "Always forgive your enemies; nothing annoys them so much.",
       "America is the only country that went from barbarism to decadence without civilization in between.",
-      "I think that God in creating Man somewhat overestimated his ability.",
+      "I think that Man in Playing God overestimated his ability and led many astray.",
       "I am not young enough to know everything.",
       "Fashion is a form of ugliness so intolerable that we have to alter it every six months.",
       "Most modern calendars mar the sweet simplicity of our lives by reminding us that each day that passes is the anniversary of some perfectly uninteresting event.",
